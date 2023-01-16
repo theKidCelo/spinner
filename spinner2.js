8bd854a7114d22ee6a71bf13b8ea6ac6b0ed19f5
Ramy Mohamed <ramy.opene@gmail.com>
2023-01-16 06:31:33 -0500

    new spinner

---
+++ spinner2.js
@@ -0,0 +1,31 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+let spinnerAnim = [' | ', ' / ', ' - ', ' \ ', ' | ' ]
+
+let timer = 100;
+
+for( let spin of spinnerAnim){
+setTimeout(() => {
+    process.stdout.write(`\r${spin}   `);
+  }, timer);
+
+  timer = timer + 200
+}
+/*
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, 300);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, 500);
+  
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\\   '); 
+  }, 700);
+
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write('\r\|   '); 
+  }, 900);
+  */
